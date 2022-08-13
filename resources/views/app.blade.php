e02319d02e478adfd4fc62df110e61900d2bf176
Alexander Matson <matsonwashere@gmail.com>
2022-08-13 02:39:07 -0400

    first commit

---
+++ resources/views/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PokeMap</title>
+
+	@vite('resources/css/app.css')
+</head>
+<body>
+	<div id="app"></div>
+    <h1>hadsadola</h1>
+    <h2>otro cambioasda</h2>
+    <h3>otro msadad</h3>
+	@vite('resources/js/app.js')
+</body>
+</html>
